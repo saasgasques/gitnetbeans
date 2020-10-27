0e5c45713c26d2208976040ed9bdd0e96b443c21
U-LAPTOP-TI4J073B\Carles <carles.gasques@gmail.com>
2020-10-27 11:56:10 +0100

    Add other files

---
+++ src/java/helloWorld/HelloWorld.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloWorld;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+/**
+ * REST Web Service
+ *
+ * @author Carles
+ */
+@Path("helloworld")
+public class HelloWorld {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of HelloWorld
+     */
+    public HelloWorld() {
+    }
+
+    // http://localhost:8080/HelloWorldApplication/webresources/helloworld
+    
+    /**
+     * Retrieves representation of an instance of helloWorld.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/xml")
+    public String getXml() {
+        //TODO return proper representation object
+        //throw new UnsupportedOperationException();
+        return "<?xml version=\"1.0\"?><hello>Hello World</hello>";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HelloWorld
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+}
